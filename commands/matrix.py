6fad43c4e8629fdf674ecc71df41304833a9b388
Roman Kudryavtsev <kudryavtsevroman@mail.ru>
2025-06-27 00:16:31 +0300

    add matrices multiplication

--- commands/matrix.py
+++ commands/matrix.py
@@ -21,3 +21,22 @@ def calc_determinant(
     print(
         f"[bold green]Matrix:[/bold green] {matrix}\n[bold yellow]Determinant:[/bold yellow] {result} :smile:"
     )
+
+
+@matrix_commands.command("multiply")
+def multiply_matrices(
+    matrix_a: Annotated[
+        str, typer.Argument(help="Use format: [[1,2,3],[4,0,6],[7,8,9]]")
+    ],
+    matrix_b: Annotated[str, typer.Argument(help="Use format: [[1,2],[3,4],[5,6]]")],
+    sym_args: Annotated[bool, typer.Option(help="Use symbolic arguments")] = False,
+    sym_res: Annotated[bool, typer.Option(help="Use symbolic calculation")] = True,
+):
+    parsed_a = args_utils.parse_2d_list_from_str(matrix_a, sym_args)
+    parsed_b = args_utils.parse_2d_list_from_str(matrix_b, sym_args)
+    result = matrix_funcs.multiply(parsed_a, parsed_b, sym_res)
+    print(
+        f"[bold green]Matrix A:[/bold green] {matrix_a}\n"
+        f"[bold green]Matrix B:[/bold green] {matrix_b}\n"
+        f"[bold yellow]Result:[/bold yellow] {result} :smile:"
+    )
--- funcs/matrix.py
+++ funcs/matrix.py
@@ -20,3 +20,24 @@ def determinant(matrix, sym_res: bool | None = False):
     else:
         numpy_matrix = numpy.array(matrix)
         return numpy.linalg.det(numpy_matrix)
+
+
+def multiply(matrix_a, matrix_b, sym_res: bool | None = False):
+    if not isinstance(matrix_a, (list, numpy.ndarray)) or not isinstance(
+        matrix_b, (list, numpy.ndarray)
+    ):
+        raise ValueError("Both inputs must be lists or numpy arrays.")
+    matrix_a = numpy.array(matrix_a)
+    matrix_b = numpy.array(matrix_b)
+    if matrix_a.shape[1] != matrix_b.shape[0]:
+        raise ValueError(
+            "Number of columns in the first matrix must equal number of rows in the second matrix."
+        )
+    if sym_res:
+        sympy_matrix_a = sympy.Matrix(matrix_a)
+        sympy_matrix_b = sympy.Matrix(matrix_b)
+        return sympy_matrix_a * sympy_matrix_b
+    else:
+        numpy_matrix_a = numpy.array(matrix_a)
+        numpy_matrix_b = numpy.array(matrix_b)
+        return numpy.dot(numpy_matrix_a, numpy_matrix_b)
